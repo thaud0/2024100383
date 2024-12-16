53a3cbaabe09fdd020c047e38dbbb0b250d2902b
thaud0 <h-bluesky@naver.com>
2024-12-16 23:34:16 +0900

    Create room.c

---
+++ 랭킹전 (3개 이상의 소스/room.c
@@ -0,0 +1,26 @@
+#include "room.h"
+#include <stdlib.h>
+#include <string.h>
+
+int find_room(Room rooms[], int room_count, int level, int M) {
+    for (int i = 0; i < room_count; i++) {
+        if (abs(rooms[i].level - level) <= 10 && rooms[i].player_count < M) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int create_new_room(Room rooms[], int *room_count, int level) {
+    rooms[*room_count].level = level;
+    rooms[*room_count].player_count = 0;
+    return (*room_count)++;
+}
+
+void add_player_to_room(Room rooms[], int room_idx, Player player) {
+    rooms[room_idx].players[rooms[room_idx].player_count++] = player;
+}
+
+int comp(const void *a, const void *b) {
+    return strcmp(((Player*)a)->name, ((Player*)b)->name);
+}
